970bb8049868fcbe1d5d2fac822270a48913980c
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-07-12 16:18:57 -0400

    The Site Class

---
+++ ssg.py
@@ -0,0 +1,12 @@
+import typer
+from ssg.site import site
+
+def main(source="content", dest="dist"):
+    config = {
+    "source" = source,
+    "dest" = dest
+    }
+
+    Site(**config).build()
+
+    typer.run()
