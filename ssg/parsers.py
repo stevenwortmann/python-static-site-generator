99bae2bd3189667c6093daf265dce289e73e04d3
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-07-13 18:19:42 -0400

    The Parser Class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,8 +3,9 @@ import shutil
 from typing import List
 from pathlib import Path
 
+
 class Parser:
-    extensions = List[str] = []
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
