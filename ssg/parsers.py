c4fb857ee403186420201185ddccac12c2fe9289
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-07-13 18:10:50 -0400

    The Parser Class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -19,7 +19,7 @@ class Parser:
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
-            return file.write(content)
+            file.write(content)
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
