ebd823ef5ada3047c0bd7bd268c00546b9360f93
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-07-14 13:33:29 -0400

    The Content Class

--- ssg/content.py
+++ ssg/content.py
@@ -7,6 +7,7 @@ class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
+    @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = load(fm, Loader=FullLoader)
